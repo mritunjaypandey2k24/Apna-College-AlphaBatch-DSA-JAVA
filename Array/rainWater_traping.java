8a2cf5914d544f790752cf132f994be018ab8e74
mritunjaypandey2k24 <mritunjaypandey.ee@gmail.com>
2024-12-28 12:26:48 +0530

    Update rainWater_traping.java

--- Array/rainWater_traping.java
+++ Array/rainWater_traping.java
@@ -1,44 +1,48 @@
-public class rainWater_traping {
-    
-    public static void trappedRainwater(int height [] ){
+public class DSA_Rainwater_Trapping {
 
-        int leftB [] = new int [height.length];
-        int rightB [] = new int [height.length]; 
-
-        //caluculate left max boundries of bar
-        leftB [0] = height [0];
-        for (int i = 1; i < height.length; i++) {
-            
-            leftB [i] = height [i-1];
+    public static int trappedRainWater(int bars[] ){
+        int n = bars.length;
+        //Calculate left max boundry 
+        int leftMax[] = new int[n];
+        leftMax [0] = bars [0];
+        
+        for(int i = 1; i< n; i++){
+            leftMax [i] = Math.max(bars[i],leftMax[i-1]); 
             
         }
+       
+        //calculate right max bondry
+        int rightMax[] = new int[n];
+        rightMax[n-1] = bars[n-1];
 
-        //calculate right max boundries of bar
-         rightB [height.length-1] = height [height.length-1];
-        for (int i = 0; i < height.length-1; i++) {
-            
-            rightB [i] = height [i+1];
-             
-
-        }
-        
-        //waterLevel = min( leftmax bound, rightmax bound)
-        
-        for (int i = 0; i < rightB.length; i++) {
-            
-            
+        for(int i = n-2; i>=0 ; i--){
+            rightMax [i] = Math.max(bars[i],rightMax[i+1]);
         }
 
         
         
+        int trappedWater = 0;
+        //loop
+        for(int i=0; i<n;i++){
+            //calculate min(left,right) = water level
+            int waterLevel = Math.min(leftMax[i],rightMax[i]);
+
+            //trapped water  = water level - height
         
-
-
+            trappedWater += waterLevel - bars[i];
+        }
+        
+        return trappedWater;
     }
 
 
-    public static void main(String args []){
-        int heigth [] = {4, 2, 0, 6, 3, 2, 5};
+
+    public static void main(String args[]){
+
+        int bars[] ={4,2,0,6,3,2,5};
+        
+        
+        System.out.println(trappedRainWater(bars));
 
 
     }
